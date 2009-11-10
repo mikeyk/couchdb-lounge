6094a29c93aac78f38528e2757e0e9fe0b9eb5f2
Randall Leeds <randall@meebo-inc.com>
2009-11-10 14:32:20 -0800

    Fix rpm config non-stomping

--- smartproxy/install.sh
+++ smartproxy/install.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
 python setup.py install --optimize=1 --root=$RPM_BUILD_ROOT --record=INSTALLED_FILES
+CONFIGFILES="%config(noreplace) /etc/lounge"
+echo "$CONFIGFILES" | cat INSTALLED_FILES - > INSTALLED_FILES.new
+mv INSTALLED_FILES.new INSTALLED_FILES
