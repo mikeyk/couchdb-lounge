4c2b635c21d68e54ddceeb73b73a75d234cacdae
Randall Leeds <randall.leeds@gmail.com>
2009-11-10 17:36:33 -0800

    More FSH standard fixes. Use /var/lib for cache

--- smartproxy/setup.py
+++ smartproxy/setup.py
@@ -11,7 +11,7 @@ init_files = ('/etc/init.d/', ['smartproxyd'])
 conf_files = ('/etc/lounge/', ['smartproxy.xml', 'smartproxy.tac'])
 check_files = ('/root/bin/', ['check-smartproxy.py'])
 cron_files = ('/etc/cron.d/', ['check-smartproxy'])
-cache_files = ('/usr/lib/lounge/smartproxy', ['cache.dat'])
+cache_files = ('/var/lib/lounge/smartproxy', ['cache.dat'])
 
 data_files = [init_files, conf_files, check_files, cron_files, cache_files]
 
